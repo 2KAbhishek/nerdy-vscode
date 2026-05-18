64cd3c1de0a9926db0aa30ddf56f927c4e04f5e6
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-05-18 12:59:05 +0530

    test: enhance extension tests and verify command registration

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -6,18 +6,20 @@ suite('Nerdy Test Suite', () => {
     test('getIconData should return icon data', () => {
         const iconData = getIconData();
         assert.ok(iconData.length > 0, 'Icon data should not be empty');
+        assert.ok(iconData[0].name, 'Icon should have a name');
+        assert.ok(iconData[0].code, 'Icon should have a code');
+        assert.ok(iconData[0].char, 'Icon should have a char');
     });
 
-    test('insertIconCommand should insert selected icon', async () => {
-        const iconData = getIconData();
-        const icon = iconData[0];
-
-        await vscode.commands.executeCommand('nerdy.insertIcon');
-
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const documentText = editor.document.getText();
-            assert.ok(documentText.includes(icon.char), 'Document should contain the inserted icon');
+    test('insertIconCommand should be registered', async () => {
+        const extensionId = 'undefined_publisher.nerdy-vscode';
+        const extension = vscode.extensions.getExtension(extensionId);
+        if (extension) {
+            await extension.activate();
+        } else {
+            console.log('Available extensions:', vscode.extensions.all.map(e => e.id));
         }
+        const commands = await vscode.commands.getCommands(true);
+        assert.ok(commands.includes('nerdy.insertIcon'), 'Command nerdy.insertIcon should be registered');
     });
 });
