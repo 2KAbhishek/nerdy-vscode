16f2882d106316c74e8df4e8675e41126f09df12
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-05-18 12:55:15 +0530

    perf: cache icon data to avoid repeated file reads

--- src/utils/iconData.ts
+++ src/utils/iconData.ts
@@ -1,16 +1,30 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-export function getIconData(): Array<{ name: string; code: string; char: string }> {
+export interface Icon {
+    name: string;
+    code: string;
+    char: string;
+}
+
+let cachedIconData: Icon[] | null = null;
+
+export function getIconData(): Icon[] {
+    if (cachedIconData) {
+        return cachedIconData;
+    }
+
     const filePath = path.join(__dirname, '../../data/glyphnames.json');
     const data = fs.readFileSync(filePath, 'utf-8');
     const jsonData: { [key: string]: { code: string; char: string } } = JSON.parse(data);
 
-    return Object.entries(jsonData)
+    cachedIconData = Object.entries(jsonData)
         .filter(([name]) => name !== 'METADATA')
         .map(([name, { code, char }]) => ({
             name,
             code,
             char,
         }));
+
+    return cachedIconData;
 }
